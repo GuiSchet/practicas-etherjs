e86c11b5c22dc8e8299140773a4bcbbabd11fc75
Guille <51679006+GuiSchet@users.noreply.github.com>
2023-10-24 18:43:55 +0000

    dotenv

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - go-ethereum se puede crear nuestro propio nodo de ethereum.
 - ethers.js es un libreria para comunicarse con la cadena:
     -   https://docs.ethers.io
+- dotenv libreria para proteger las claves privadas. 
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const ethers = require('ethers');
-
+require('dotenv').config();
 //voy a usar de prueba la red Goerli.
 
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider('https://goerli.infura.io/v3/');
-    const wallet = new ethers.Wallet('aca va la clave privada', provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 };
